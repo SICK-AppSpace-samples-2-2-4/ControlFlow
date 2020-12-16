b4bd0c7fbacb62d6c936272f0bc0e4558f2823d1
rdzanmi <mikolaj.rdzanek@sick.de>
2020-12-16 18:57:35 +0100

    Initial commit

---
+++ ControlFlow/scripts/FlowsDescription.lua
@@ -0,0 +1,22 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name:
+  ControlFlow
+  
+  Summary:
+  Creating control flows
+  
+  Description:
+  Introduction sample for creating control flows. An output state is changed with
+  a delay, depending on the state of two input ports.
+  In the control Flow editor a DigitalLogic "and" Gate receives the signal from two
+  Input OnChange connectors. The DigitalOut is then set 2s delayed, depending on the
+  logic of the "and" gate.
+    
+  How to run:
+  To run this sample the FullFeatured emulator can be used. The state of the Input ports
+  can be toggled using the keyboard key "1" and "2" inside the Emulator tab. It has
+  to be noted, that the emulator does not show when the states are unchanged.
+  Before opening the flow editor, the Emulator or device must be connected.
+  
+------------------------------------------------------------------------------]]
---
+++ readme.md
@@ -0,0 +1,20 @@
+## ControlFlow
+Creating control flows
+
+### Description
+Introduction sample for creating control flows. An output state is changed with
+a delay, depending on the state of two input ports.
+In the control Flow editor a DigitalLogic "and" Gate receives the signal from two
+Input OnChange connectors. The DigitalOut is then set 2s delayed, depending on the
+logic of the "and" gate.
+
+### How to Run
+To run this sample the FullFeatured emulator can be used. The state of the Input ports
+can be toggled using the keyboard key "1" and "2" inside the Emulator tab. It has
+to be noted, that the emulator does not show when the states are unchanged.
+Before opening the flow editor, the Emulator or device must be connected.
+
+
+
+### Topics
+Getting-Started, Flow, Sample, SICK-AppSpace
